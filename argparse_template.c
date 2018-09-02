cff957f86c97d9076bcc3d9f1e3ce35ed281da6c
Johannes Bauer <joe@johannes-bauer.com>
2018-09-02 14:17:18 +0200

    Added support for positional arguments
    Previously we didn't need this, but we do now: nargs = "+" and "*" are
    supported.

--- argparse_template.c
+++ argparse_template.c
@@ -35,7 +35,7 @@ bool argparse_parse(int argc, char **argv, argparse_callback_t argument_callback
 		}
 		enum argparse_option_internal_t arg = (enum argparse_option_internal_t)optval;
 		switch (arg) {
-%for opt in opts:
+%for opt in [ opt for opt in opts if not opt.positional ]:
 %if opt.opt_short is not None:
 			case ARG_${opt.name.upper()}_SHORT:
 %endif
@@ -53,7 +53,37 @@ bool argparse_parse(int argc, char **argv, argparse_callback_t argument_callback
 		}
 	}
 
-	/* TODO: Handle positional arguments */
+	const int positional_argument_cnt = argc - optind;
+%if have_flexible_positional_argument:
+	const int flexible_positional_args_cnt = positional_argument_cnt - ${sum(1 for opt in opts if (opt.positional and (opt.nargs is None)))};
+%endif
+%if min_pos is not None:
+	if (positional_argument_cnt < ${min_pos}) {
+		fprintf(stderr, "error: expected a minimum of ${min_pos} positional arguments, %d given.\n", positional_argument_cnt);
+		return false;
+	}
+%endif
+%if max_pos is not None:
+	if (positional_argument_cnt > ${max_pos}) {
+		fprintf(stderr, "error: expected a maximum of ${max_pos} positional arguments, %d given.\n", positional_argument_cnt);
+		return false;
+	}
+%endif
+
+	int positional_index = optind;
+%for opt in [ opt for opt in opts if opt.positional ]:
+	%if opt.nargs is None:
+	if (!argument_callback(ARG_${opt.name.upper()}, argv[positional_index++])) {
+		return false;
+	}
+	%else:
+	for (int i = 0; i < flexible_positional_args_cnt; i++) {
+		if (!argument_callback(ARG_${opt.name.upper()}, argv[positional_index++])) {
+			return false;
+		}
+	}
+	%endif
+%endfor
 
 	return true;
 }
@@ -70,3 +100,29 @@ void argparse_parse_or_die(int argc, char **argv, argparse_callback_t argument_c
 		exit(EXIT_FAILURE);
 	}
 }
+
+#ifdef __ARGPARSE_MAIN__
+/*   gcc -D __ARGPARSE_MAIN__ -O2 -Wall -o argparse argparse.c
+*/
+
+static const char *option_enum_to_str(enum argparse_option_t option) {
+	switch (option) {
+%for opt in opts:
+%if opt.opt_long is not None:
+		case ARG_${opt.name.upper()}: return "ARG_${opt.name.upper()}";
+%endif
+%endfor
+	}
+	return "UNKNOWN";
+}
+
+bool arg_print_callback(enum argparse_option_t option, const char *value) {
+	fprintf(stderr, "%s = \"%s\"\n", option_enum_to_str(option), value);
+	return true;
+}
+
+int main(int argc, char **argv) {
+	argparse_parse_or_die(argc, argv, arg_print_callback);
+	return 0;
+}
+#endif
